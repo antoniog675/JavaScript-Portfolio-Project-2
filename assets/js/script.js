f41bcacd8bfc1d6031b89b28a25bf489e043d549
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-03-04 14:00:18 +0000

    Added pop up message to index.html, code is withing the js script with comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// For pop up message of the rules
+let c = 0;
+function pop() {
+    if(c == 0) {
+        document.getElementById("rules-message-box").style.display = "block";
+        c = 1;
+    }
+    else {
+        document.getElementById("rules-message-box").style.display = "none";
+        c = 0;
+    }
+}
+
+// Main JS
+
 var questions = [
     {
 
